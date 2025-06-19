201fd5c54154e9283c2204a5b49ebc8d149f2ccb
ST10476490 <St10476490@vcconnect.edu.za>
2025-06-19 11:26:43 +0200

    Initial commit

---
+++ app/src/main/java/vcmsa/ci/songapp2025/MainActivity2.kt
@@ -0,0 +1,35 @@
+package vcmsa.ci.songapp2025
+
+import android.os.Bundle
+import android.view.Display
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+        val button = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+
+private infix fun Any.contentView(activityMain2: Any) {
+    TODO("Not yet implemented")
+}
+
+val btn = findViewById<Button>(R.id.btn11)
+
+
+
